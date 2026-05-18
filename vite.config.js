ca52e1a48097e2cdaa7e7338ef013a5b40882b3c
Deiyox22 <anthony.engels2259@gmail.com>
2026-05-18 21:02:01 +0200

    Employé view

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       devOptions: {
         enabled: true
       },
-      includeAssets: ['icons/icon-192.png', 'icons/icon-512.png'],
+      includeAssets: ['icons/icon.svg'],
       manifestFilename: 'manifest.json',
       manifest: {
         name: 'AgriClean Manager',
@@ -25,14 +25,9 @@ export default defineConfig({
         orientation: 'portrait-primary',
         icons: [
           {
-            src: 'icons/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: 'icons/icon.svg',
+            sizes: '192x192 512x512',
+            type: 'image/svg+xml',
             purpose: 'any maskable'
           }
         ]
